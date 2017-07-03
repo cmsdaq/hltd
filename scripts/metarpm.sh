e39b99e9d57c64176467c4d0604e653df0cc7c34
Srecko Morovic <srecko.morovic@cern.ch>
2017-07-03 12:03:34 +0200

    version update

--- scripts/metarpm.sh
+++ scripts/metarpm.sh
@@ -188,7 +188,7 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > fffmeta.spec <<EOF
 Name: $PACKAGENAME
-Version: 2.2.0
+Version: 2.2.1
 Release: 0${revsuffix}
 Summary: hlt daemon
 License: gpl
