30294f6f367ba3e1be23f428aa99cc7c510880c5
Srecko Morovic <srecko.morovic@cern.ch>
2018-06-21 10:55:27 +0200

    change number of restarts from 5 to 1

--- scripts/metarpm.sh
+++ scripts/metarpm.sh
@@ -211,7 +211,7 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > fffmeta.spec <<EOF
 Name: $PACKAGENAME
-Version: 2.2.5
+Version: 2.2.8
 Release: 0${revsuffix}
 Summary: hlt daemon
 License: gpl
